106f2ea8d6e827e44b570c6b2760c353c5cbb14e
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2024-06-06 09:58:51 +0530

    [wip]

--- local-library/controllers/bookController.js
+++ local-library/controllers/bookController.js
@@ -1,13 +1,43 @@
 const asyncHandler = require('express-async-handler');
 
 const Book = require('../models/book');
+const Author = require('../models/author');
+const Genre = require('../models/genre');
+const BookInstance = require('../models/bookinstance');
 
 const index = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Site Home Page');
+  const [
+    numBooks,
+    numBookInstances,
+    numAvailableBookInstances,
+    numAuthors,
+    numGenres
+  ] = await Promise.all([
+    Book.countDocuments({}).exec(),
+    BookInstance.countDocuments({}).exec(),
+    BookInstance.countDocuments({ status: 'Available' }).exec(),
+    Author.countDocuments({}).exec(),
+    Genre.countDocuments({}).exec()
+  ]);
+
+  // res.send('NOT IMPLEMENTED: Site Home Page');
+  res.render('index', {
+    title: 'Local Library Home',
+    book_count: numBooks,
+    book_instance_count: numBookInstances,
+    book_instance_available_count: numAvailableBookInstances,
+    author_count: numAuthors,
+    genre_count: numGenres
+  });
 });
 
 const book_list = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Book list');
+  // res.send('NOT IMPLEMENTED: Book list');
+  const allBooks = await Book.find({}, 'title author')
+    .sort({ title: 1 })
+    .populate('author')
+    .exec();
+  res.render('book_list', { title: 'Book List', book_list: allBooks });
 });
 
 const book_detail = asyncHandler(async (req, res, next) => {
